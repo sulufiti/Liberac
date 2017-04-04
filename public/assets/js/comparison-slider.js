fc5e17d5cd03f1c9b744c19d5fd9134f0b18274a
Marcus Crane <marcus@thingsima.de>
2017-04-05 11:19:41 +1200

    Added competitor values

--- public/assets/js/comparison-slider.js
+++ public/assets/js/comparison-slider.js
@@ -1,21 +1,39 @@
+// Transfer Funds Slider elements
 var transferSlider = document.querySelector('#transfer-slider')
 var transferReadout = document.querySelector('#transfer-value')
 
+// Pricing display elements
 var liberacPricing = document.querySelector('#liberac_pricing')
 var competitorPricing = document.querySelector('#competitor_pricing')
+var selectedCompetitor = document.querySelector('#competitor_options')
+
+// Helper bits
 
 transferSlider.onchange = function() {
   transferReadout.innerHTML = '$' + transferSlider.value
   liberacPricing.value = getLiberacPrice(transferSlider.value)
-  competitorPricing.value = getCompetitorPricing(transferSlider.value)
+
+  switch(selectedCompetitor.value) {
+    case 'clickex':
+      competitorPricing.value = getClickexPrice(transferSlider.value)
+      break
+    case 'westernunion':
+      competitorPricing.value = getWesternUnionPrice(transferSlider.value)
+      break
+    default:
+      console.log('No competitor selected?!')
+  }
 }
 
 function getLiberacPrice(value) {
-  value = parseInt(value)
-  return '$' + (value + 10)
+  return '$' + (parseInt(value) + 10)
 }
 
-function getCompetitorPricing(value) {
-  value = parseInt(value)
-  return '$' + (value + 30)
+function getClickexPrice(value) {
+  return '$' + (parseInt(value) + 30)
 }
+
+function getWesternUnionPrice(value) {
+  return '$' + (parseInt(value) + 999)
+}
+

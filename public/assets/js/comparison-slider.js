3b36321181e1f9b950f29f4237ac437d418d14e1
Marcus Crane <marcus@thingsima.de>
2017-04-05 12:52:50 +1200

    Lunchtime

--- public/assets/js/comparison-slider.js
+++ public/assets/js/comparison-slider.js
@@ -7,47 +7,72 @@ var liberacPricing = document.querySelector('#liberac_pricing')
 var competitorPricing = document.querySelector('#competitor_pricing')
 var selectedCompetitor = document.querySelector('#competitor_options')
 
-// Helper bits
-
+// Update the pricing displays
 transferSlider.onchange = function() {
   transferReadout.innerHTML = '$' + transferSlider.value
   liberacPricing.value = getLiberacPrice(transferSlider.value)
-
   updateCompetitorPrice(selectedCompetitor.value)
 }
 
+// Update the pricing when flicking between competitors
 selectedCompetitor.onchange = function() {
   updateCompetitorPrice(selectedCompetitor.value)
 }
 
-function getLiberacPrice(value) {
-  return '$' + (parseInt(value) + 10)
-}
+// KlickEx
+// Western Union
+// Moneygram
+// Pacific Ezy
+// ANZ
 
-function getKlickexPrice(value) {
-  return '$' + (parseInt(value) + 30)
-}
-
-function getSMPPrice(value) {
-  return '$' + (parseInt(value) + 50)
-}
-
-function getWesternUnionPrice(value) {
-  return '$' + (parseInt(value) + 999)
-}
-
-function updateCompetitorPrice(competitor) {
-  switch(competitor) {
+function getPrice(company, value) {
+  switch(company) {
+    case 'liberac':
+      return '$' + (value + 100)
+      break
     case 'klickex':
-      competitorPricing.value = getKlickexPrice(transferSlider.value)
+      return '$' + (value + 200)
       break
     case 'westernunion':
-      competitorPricing.value = getWesternUnionPrice(transferSlider.value)
+      return '$' + (value + 300)
       break
-    case 'sendmoneypacific':
-      competitorPricing.value = getSMPPrice(transferSlider.value)
+    case 'moneygram':
+      return '$' + (value + 400)
+      break
+    case 'pacificezy':
+      return '$' + (value + 500)
+      break
+    case 'anz':
+      return '$' + (value + 600)
+      break
     default:
-      console.log('No competitor selected?!')
+      console.error('No competitor selected!')
+      break
+  }
+}
+
+function updateCompetitorPrice(company) {
+  switch(company) {
+    case 'liberac':
+      liberacPricing.value = getPrice(company, parseInt(transferSlider.value))
+    case 'klickex':
+      competitorPricing.value = getPrice(company, parseInt(transferSlider.value))
+      break
+    case 'westernunion':
+      competitorPricing.value = getPrice(company, parseInt(transferSlider.value))
+      break
+    case 'moneygram':
+      competitorPricing.value = getPrice(company, parseInt(transferSlider.value))
+      break
+    case 'pacificezy':
+      competitorPricing.value = getPrice(company, parseInt(transferSlider.value))
+      break
+    case 'anz':
+      competitorPricing.value = getPrice(company, parseInt(transferSlider.value))
+      break
+    default:
+      console.error('No competitor to update?')
+      break
   }
 }
 

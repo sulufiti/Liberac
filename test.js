738b98f17a1a6b6a24cfebdd2cf51048e9445c96
Marcus Crane <marcus@thingsima.de>
2017-04-06 10:31:35 +1200

    Fiddled with example for PoC

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 knex('users')
 .then((users) => {
   for (user of users) {
-    console.log('person', user)
+    console.log(`${user.first_name} ${user.last_name} lives at ${user.address}`)
   }
 })
 .then(() => {

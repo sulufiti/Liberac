b2eaed58ca168227306e54a7287c5475aef11821
Marcus Crane <marcus@thingsima.de>
2017-04-06 17:18:08 +1200

    Added super basic registration flow

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
+// Basic Express requirements
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+// Setting up express middlewares
 const app = express()
 const router = express.Router()
 const jsonParser = bodyParser.json()
 const port = 3000
 
+// Routes
 const index = require('./routes/index')
 
+// Template rendering
 app.set('views', path.join(__dirname, 'views'))
-
+app.set('view engine', 'hbs')
 app.locals.pretty = true
 
+// Enabling middlewares
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Serve static files and routes that use templates
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', index)
 
+// Error handlers
 app.use((req, res, next) => {
   let err = new Error('Not Found')
   err.status = 404
@@ -29,6 +37,7 @@ app.use((err, req, res, next) => {
   res.send(err)
 })
 
+// Listen on a server defined port if it exists
 app.listen(process.env.PORT || port, () => {
   console.log(`Liberac is now being served at http://localhost:${port}`)
 })

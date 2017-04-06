7eb1aa7aa7e068a3d43585672f69a0595e444108
Marcus Crane <marcus@thingsima.de>
2017-04-06 12:17:03 +1200

    Added some basic file upload and db tests for later. Actual tests will need to be written

---
+++ file-test.js
@@ -0,0 +1,23 @@
+// const azure = require('azure-storage')
+// const fs = require('fs')
+// const blobService = azure.createBlobService(process.env.AZURE_STORAGE_ACCOUNT, process.env.AZURE_STORAGE_ACCESS_KEY)
+
+// blobService.getBlobToStream('passports', 'b2b1461a-9fac-4c22-94b5-12b7b0ac2c20', fs.createWriteStream('LICENSE.txt'), (error, result, response) => {
+//   if (!error) {
+//     console.log('Retrieved blob as returntrip.gif')
+//   }
+// })
+
+// blobService.createBlockBlobFromLocalFile('passports', 'b2b1461a-9fac-4c22-94b5-12b7b0ac2c20', './LICENSE.txt', (error, result, response) => {
+//   if (!error) {
+//     console.log(result)
+//     console.log(response)
+//   }
+// })
+
+// blobService.createBlockBlobFromLocalFile('addressproofs', 'b2b1461a-9fac-4c22-94b5-12b7b0ac2c20', './filestoragetest.gif', (error, result, response) => {
+//   if (!error) {
+//     console.log(result)
+//     console.log(response)
+//   }
+// })
---
+++ tests/db_test.js
@@ -0,0 +1,16 @@
+// const Knex = require('knex')
+// const knexConfig = require('../knexfile')
+// const knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
+
+// knex('users')
+// .then((users) => {
+//   for (user of users) {
+//     console.log(`${user.first_name} ${user.last_name} lives at ${user.address}`)
+//   }
+// })
+// .then(() => {
+//   process.exit(0)
+// })
+// .catch((err) => {
+//   console.log(err)
+// })

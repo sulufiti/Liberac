6b4834782b24e813796aa286e8ebb4f7e5c5c9dd
Marcus Crane <marcus@thingsima.de>
2017-04-07 12:06:20 +1200

    Setup express session in express config

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-// Basic Express requirements
+// Express imports
+const env = require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const session = require('express-session')
 
 // Setting up express middlewares
 const app = express()
-const router = express.Router()
 const jsonParser = bodyParser.json()
 const port = 3000
+const router = express.Router()
+let sessionSettings = {
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}
 
 // Routes
 const index = require('./routes/index')
@@ -17,9 +25,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 app.locals.pretty = true
 
+// Non-secure cookie if in development mode
+if (app.get('env') === 'development') {
+  sessionSettings.cookie.secure = false
+}
+
 // Enabling middlewares
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session(sessionSettings))
+
 
 // Serve static files and routes that use templates
 app.use(express.static(path.join(__dirname, 'public')))
@@ -40,8 +55,8 @@ app.use((err, req, res, next) => {
 
 // Listen on a server defined port if it exists
 app.listen(process.env.PORT || port, () => {
-  console.log(`Liberac is now being served at http://localhost:${port}\n`)
-  console.log(`Running in ${process.env.NODE_ENV} mode`)
+  console.log(`Liberac is now being served at http://localhost:${port}`)
+  console.log(`Running in ${app.get('env').toUpperCase()} mode`)
 })
 
 module.exports = app

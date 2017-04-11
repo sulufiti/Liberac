59f1de020514ae99a03a25fce81a65e3d3be5339
Marcus Crane <marcus@thingsima.de>
2017-04-11 22:57:17 +1200

    Basic file upload works

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const router = express.Router()
 const passport = require('passport')
 const bcrypt = require('bcrypt')
+const azure = require('azure-storage')
 const user = require('../helpers/db_users.js')
+const router = express.Router()
+const blobService = azure.createBlobService(process.env.AZURE_STORAGE_ACCOUNT, process.env.AZURE_STORAGE_ACCESS_KEY)
+
 const saltRounds = 10
 
 router.get('/register', (req, res, next) => {
@@ -47,11 +50,29 @@ router.get('/loggedout', (req, res, next) => {
 })
 
 router.get('/fileupload', (req, res, next) => {
-  res.render('fileupload')
+  user.findByUsername('samuel1234')
+  .then(user => {
+    res.render('fileupload', { id: user.id })
+  })
 })
 
 router.post('/fileupload', (req, res, next) => {
-  console.log(req.files)
+  let user = { 'user': req.body.userid, 'passport': req.files.passport.data, 'address_proof': req.files.addressproof.data }
+  blobService.createBlockBlobFromText('passports', req.body.userid, req.files.passport.data, (error, result, response) => {
+    if (!error) {
+      console.log(result)
+      console.log(response)
+      res.send('file uploaded')
+    }
+  })
+})
+
+router.get('/listblobs', (req, res, next) => {
+  blobService.getBlobToText('passports', 'aa26b364-07d3-4f1f-b63e-f04234e2504b', (error, blobContent, blob) => {
+    if (!error) {
+      
+    }
+  })
 })
 
 module.exports = router

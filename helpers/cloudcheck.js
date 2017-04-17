71314f7c7603c1bbb689817fdf850a5f0a160695
Marcus Crane <marcus@thingsima.de>
2017-04-18 10:57:18 +1200

    Middlename and suburb are optional

--- helpers/cloudcheck.js
+++ helpers/cloudcheck.js
@@ -10,14 +10,12 @@ const verifyUser = (user, nonce) => {
   const data = {
     "details": {
       "address": {
-        "suburb": user.suburb,
         "street": user.street,
         "postcode": user.postcode,
         "city": user.city
       },
       "name": {
         "given": user.first_name,
-        "middle": user.middle_name,
         "family": user.last_name
       },
       "driverslicence": {},
@@ -34,6 +32,9 @@ const verifyUser = (user, nonce) => {
     "reference": "3",
     "consent": "Yes"
   }
+
+  if (user.middlename) { data.name["middle"] = user.middlename }
+  if (user.suburb) { data.address["suburb"] = user.suburb }
 
   const current_time = Date.now()
 

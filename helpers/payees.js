8de950226f0fcf1fc8768c85705303cb276a55e9
Marcus Crane <marcus@thingsima.de>
2017-04-19 11:46:02 +1200

    Added payees migration fix

--- helpers/payees.js
+++ helpers/payees.js
@@ -3,10 +3,34 @@ const knexConfig = require('../knexfile')
 const knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 
 const addPayee = (userID, payee) => {
-  return knex('friends')
-  .returning('accepted')
-  .insert({ user_a: userID, user_b: friendID })
+  return knex('payees')
+  .insert({ 
+    user_id: userID,
+    nickname: payee.nickname,
+    first_name: payee.first_name,
+    middle_name: payee.middle_name,
+    last_name: payee.last_name,
+    phone: payee.phone,
+    email: payee.email,
+    street: payee.street,
+    suburb: payee.suburb,
+    city: payee.city,
+    postcode: payee.postcode
+  })
   .catch((err) => {
     console.error('error adding friend', err)
   })
+}
+
+const getUsersPayees = (userID) => {
+  return knex('payees')
+  .where('user_id', userID)
+  .catch((err) => {
+    console.error('error fetching payees', err)
+  }
+}
+
+module.exports = {
+  addPayee,
+  getUsersPayees
 }

ec853375ed530f18afbd87b5ba695dba9d093626
Marcus Crane <marcus@thingsima.de>
2017-04-19 18:24:36 +1200

    Started cleaning up standard errors

--- helpers/cloudcheck.js
+++ helpers/cloudcheck.js
@@ -4,38 +4,38 @@ const hmac = crypto.createHmac('sha256', process.env.CLOUDCHECK_SECRET)
 
 const verifyUser = (user, nonce) => {
   let data = {
-    "details": {
-      "address": {
-        "street": user.street,
-        "postcode": user.postcode,
-        "city": user.city
+    'details': {
+      'address': {
+        'street': user.street,
+        'postcode': user.postcode,
+        'city': user.city
       },
-      "name": {
-        "given": user.first_name,
-        "family": user.last_name
+      'name': {
+        'given': user.first_name,
+        'family': user.last_name
       },
-      "driverslicence": {},
-      "passport": {
-        "number": user.passport_number,
-        "expiry": user.passport_expiry
+      'driverslicence': {},
+      'passport': {
+        'number': user.passport_number,
+        'expiry': user.passport_expiry
       },
-      "citizenship": {},
-      "country": {},
-      "birthcertificate" : {},
-      "vehicle": {},
-      "dateofbirth": user.dateofbirth
+      'citizenship': {},
+      'country': {},
+      'birthcertificate' : {},
+      'vehicle': {},
+      'dateofbirth': user.dateofbirth
     },
-    "reference": "3",
-    "consent": "Yes"
+    'reference': '3',
+    'consent': 'Yes'
   }
 
   if (user.suburb) { data.details.address.suburb = user.suburb }
   if (user.middle_name) { data.details.name.middle = user.middle_name }
 
-  const current_time = Date.now()
+  const currentTime = Date.now()
   console.log(data)
 
-  hmac.update(`/verify/data=${JSON.stringify(data)};key=${process.env.CLOUDCHECK_API_KEY};nonce=${nonce};timestamp=${current_time};`)
+  hmac.update(`/verify/data=${JSON.stringify(data)};key=${process.env.CLOUDCHECK_API_KEY};nonce=${nonce};timestamp=${currentTime};`)
   const cloudsignature = hmac.digest('hex')
 
   return got.post('https://api.cloudcheck.co.nz/verify/', {

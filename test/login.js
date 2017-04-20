536029382568694d2eff16343714f007b49ea47d
Marcus Crane <marcus@thingsima.de>
2017-04-20 13:57:54 +1200

    Added login tests

--- test/login.js
+++ test/login.js
@@ -1,68 +1,73 @@
 const Nightmare = require('nightmare')
 const expect = require('chai').expect
 require('dotenv').config({path: '../.env'})
-// const server = require('../server')
 
+describe('Login tests', function() {
+  this.timeout(0)
+  let nightmare = null
 
-describe('Login page rejects upon incorrect details', function() {
-  it('should be redirected back to login upon incorrect username', function (done) {
-    this.timeout(0)
-    const nightmare = Nightmare()
-    nightmare
-      .goto('http://localhost:3000/login')
-      .type('#username', 'notarealuser')
-      .type('#password', 'notarealpassword')
-      .click('#login')
-      .wait(500)
-      .url()
-      .end()
-      .then((url) => {
-        expect(url).to.equal('http://localhost:3000/login')
-        done()
-      })
-      .catch((err) => {
-        console.log('wrong username, wrong pass failed', err)
-      })
+  beforeEach(() => {
+    nightmare = new Nightmare({ show: false })
   })
-  
-  it('should be redirected back to login upon incorrect username', function (done) {
-    this.timeout(0)
-    const nightmare = Nightmare()
-    nightmare
-      .goto('http://localhost:3000/login')
-      .insert('#username', 'notarealuser')
-      .insert('#password', 'notarealpassword')
-      .click('#login')
-      .wait(500)
-      .url()
-      .end()
-      .then((url) => {
-        expect(url).to.equal('http://localhost:3000/login')
-        done()
-      })
-      .catch((err) => {
-        console.log('wrong username, wrong pass failed', err)
-      })
-  })
-})
 
-describe('login page functions as expected', function() {
-  it('login in with authentic details'), function(done) {
-    this.timeout(0)
-    const nightmare = Nightmare()
-    nightmare
-      .goto('http://localhost:3000/login')
-      .insert('#username', 'george1234')
-      .insert('#password', 'george1234')
-      .click('#login')
-      .url()
-      .end()
-      .then((url) => {
-        expect(url).to.equal('http://localhost:3000/dashboard')
-        done()
-      })
-      .catch((err) => {
-        console.log('failed to login in properly')
-      })
-  }
+  describe('Login page rejects upon incorrect details', function() {
+    it('redirected back to login upon incorrect username', function(done) {
+      this.timeout(0)
+      nightmare
+        .goto('http://localhost:3000/login')
+        .insert('#username', 'notarealuser')
+        .insert('#password', 'notarealpassword')
+        .click('#login')
+        .wait(500)
+        .url()
+        .end()
+        .then((url) => {
+          expect(url).to.equal('http://localhost:3000/login')
+          done()
+        })
+        .catch((err) => {
+          console.log('wrong username, wrong pass failed', err)
+        })
+    })
+    
+    it('redirected back to login upon incorrect username', function(done) {
+      this.timeout(0)
+      nightmare
+        .goto('http://localhost:3000/login')
+        .insert('#username', 'notarealuser')
+        .insert('#password', 'notarealpassword')
+        .click('#login')
+        .wait(500)
+        .url()
+        .end()
+        .then((url) => {
+          expect(url).to.equal('http://localhost:3000/login')
+          done()
+        })
+        .catch((err) => {
+          console.log('wrong username, wrong pass failed', err)
+        })
+    })
+  })
+
+  describe('Login page... logins in with correct user details', function() {
+    it('logs in with correct details', function(done) {
+      this.timeout(0)
+      nightmare
+        .goto('http://localhost:3000/login')
+        .insert('#username', 'george1234')
+        .insert('#password', 'george1234')
+        .click('#login')
+        .wait(500)
+        .url()
+        .end()
+        .then((url) => {
+          expect(url).to.equal('http://localhost:3000/dashboard')
+          done()
+        })
+        .catch((err) => {
+          console.log('wrong username, wrong pass failed', err)
+        })
+    })
+  })
 })

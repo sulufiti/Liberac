17107a1b4fcc80d0f72b87bf7f0dec823c191529
Marcus Crane <marcus@thingsima.de>
2017-04-20 12:27:08 +1200

    Added login tests

--- test/login.js
+++ test/login.js
@@ -1,24 +1,68 @@
 const Nightmare = require('nightmare')
 const expect = require('chai').expect
-const got = require('got')
 require('dotenv').config({path: '../.env'})
-const server = require('../server')
+// const server = require('../server')
 
-describe('Login page works as expected', function() {
-  beforeEach(function() {
-    server.listen(3000)
+
+describe('Login page rejects upon incorrect details', function() {
+  it('should be redirected back to login upon incorrect username', function (done) {
+    this.timeout(0)
+    const nightmare = Nightmare()
+    nightmare
+      .goto('http://localhost:3000/login')
+      .type('#username', 'notarealuser')
+      .type('#password', 'notarealpassword')
+      .click('#login')
+      .wait(500)
+      .url()
+      .end()
+      .then((url) => {
+        expect(url).to.equal('http://localhost:3000/login')
+        done()
+      })
+      .catch((err) => {
+        console.log('wrong username, wrong pass failed', err)
+      })
   })
-  afterEach(function() {
-    server.close()
+  
+  it('should be redirected back to login upon incorrect username', function (done) {
+    this.timeout(0)
+    const nightmare = Nightmare()
+    nightmare
+      .goto('http://localhost:3000/login')
+      .insert('#username', 'notarealuser')
+      .insert('#password', 'notarealpassword')
+      .click('#login')
+      .wait(500)
+      .url()
+      .end()
+      .then((url) => {
+        expect(url).to.equal('http://localhost:3000/login')
+        done()
+      })
+      .catch((err) => {
+        console.log('wrong username, wrong pass failed', err)
+      })
   })
-  it('is running', function testSlash(done) {
-    got('http://localhost:3000/login')
-    .then((res) => {
-      expect(res.statusCode).to.equal(200)
-      done()
-    })
-    .catch(err => {
-      console.log('server offline')
-    })
-  })
+})
+
+describe('login page functions as expected', function() {
+  it('login in with authentic details'), function(done) {
+    this.timeout(0)
+    const nightmare = Nightmare()
+    nightmare
+      .goto('http://localhost:3000/login')
+      .insert('#username', 'george1234')
+      .insert('#password', 'george1234')
+      .click('#login')
+      .url()
+      .end()
+      .then((url) => {
+        expect(url).to.equal('http://localhost:3000/dashboard')
+        done()
+      })
+      .catch((err) => {
+        console.log('failed to login in properly')
+      })
+  }
 })

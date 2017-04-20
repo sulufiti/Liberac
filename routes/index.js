170ec79dc892f8346524af54c16488d2d4660e49
Marcus Crane <marcus@thingsima.de>
2017-04-20 16:56:34 +1200

    Fix indentation on form

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const Knex = require('knex')
+const knexConfig = require('../knexfile')
+const knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 
 router.get('/', (req, res, next) => {
   res.render('index')

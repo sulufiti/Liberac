ef9b0767ce4c127f29b86a7bf09a6c4845da2421
Marcus Crane <marcus@thingsima.de>
2017-04-26 16:06:25 +1200

    Updated registration helper in DB

--- helpers/users.js
+++ helpers/users.js
@@ -4,25 +4,14 @@ const knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 const moment = require('moment')
 const uuidV4 = require('uuid/v4')
 
-const register = (registration) => {
+module.exports.register = (registration) => {
   let user = {
     id: uuidV4(),
-    username: registration.username,
+    email: registration.email,
     password: registration.password,
     first_name: registration.first_name,
-    last_name: registration.last_name,
-    phone: registration.phone,
-    email: registration.email,
-    street: registration.street,
-    city: registration.city,
-    postcode: registration.postcode,
-    dateofbirth: moment(registration.dateofbirth).format('YYYY-MM-DD'),
-    accepted_agreement: true
+    last_name: registration.last_name
   }
-
-  // Optional fields
-  if (registration.middle_name) { user['middle_name'] = registration.middle_name }
-  if (registration.suburb) { user['suburb'] = registration.suburb }
 
   return knex('users')
   .insert(user)
@@ -31,7 +20,6 @@ const register = (registration) => {
 const findByUsername = (username) => {
   return knex('users').where('username', username)
   .then((user) => { return user[0] })
-  .catch((err) => { console.error(err) })
 }
 
 const findByID = (id) => {
@@ -60,6 +48,5 @@ module.exports = {
   appendIDproof,
   activateUser,
   findByUsername,
-  findByID,
-  register
+  findByID
 }
--- routes/auth.js
+++ routes/auth.js
@@ -22,22 +22,7 @@ router.post('/register', (req, res, next) => {
     })
   })
   .catch((err) => {
-    Raven.captureException(err, {
-        user: {
-          username: req.body.username,
-          password: req.body.password,
-          firstName: req.body.first_name,
-          middleName: req.body.middle_name,
-          lastName: req.body.last_name,
-          phone: req.body.phone,
-          email: req.body.email,
-          street: req.body.street,
-          suburb: req.body.suburb,
-          dateofbirth: req.body.dateofbirth,
-          city: req.body.city,
-          postcode: req.body.city
-        }
-      })
+    Raven.captureException(err, { user: req.body })
   })
 })
 
@@ -54,9 +39,7 @@ router.get('/activate/:id', (req, res, next) => {
   })
   .catch((err) => {
     Raven.captureException(err, {
-      user: {
-        id: req.params.id
-      }
+      user: { id: req.params.id }
     })
   })
 })

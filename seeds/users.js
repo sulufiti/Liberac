5355b5e93caa9150f7e38795e4db9d594fa80037
Marcus Crane <marcus@thingsima.de>
2017-04-26 16:08:46 +1200

    Updated seed files

--- seeds/users.js
+++ seeds/users.js
@@ -8,41 +8,20 @@ exports.seed = function (knex, Promise) {
       return Promise.all([
         knex('users').insert({
           id: uuidV4(),
-          username: 'steve1234',
+          email: 'steve@smith.com',
           password: '$2a$10$fZz7SR8q4CK67CJQ1zeuduvL2Pm9uB3fO8JxlorjWixpeZ5cNeVZC',
           first_name: 'Steve',
-          middle_name: 'Ashley',
-          last_name: 'Smith',
-          phone: '0262946302',
-          email: 'SteveSmith@armyspy.com',
-          street: 'Flat 3, 12 Albert Street',
-          suburb: 'Otahuhu',
-          city: 'Auckland',
-          postcode: '1062',
-          dateofbirth: '1994-01-12',
-          passport_number: 'PP888777',
-          passport_expiry: '2017-02-22',
-          accepted_agreement: true
+          last_name: 'Smith'
         }),
 
         knex('users').insert({
           id: uuidV4(),
           username: 'george1234',
+          email: 'george@bush.com',
           password: '$2a$10$s3El2HAbmTP1fB72kEXms.HbhDJ31kydURuqHqAnoz/cYzmWamGB2',
           balance: 1234.56,
           first_name: 'George',
-          middle_name: 'Walker',
           last_name: 'Bush',
-          phone: '0212345678',
-          email: 'George@expresident.org',
-          street: '1600 Clintons Road',
-          suburb: 'RD1',
-          city: 'Darfield',
-          postcode: '7571',
-          dateofbirth: '1946-07-06',
-          passport_number: 'US112233',
-          passport_expiry: '2020-02-20',
-          accepted_agreement: true,
           activated: true
         })
       ])

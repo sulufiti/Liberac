be6bd2d524a043b744380de8ec6a94e6c3ed539a
Marcus Crane <marcus@thingsima.de>
2017-04-27 17:04:00 +1200

    Confirm amount page is setup

--- routes/send.js
+++ routes/send.js
@@ -4,7 +4,7 @@ const Raven = require('raven')
 const users = require('../helpers/users')
 const contacts = require('../helpers/contacts')
 
-router.get('/send', (req, res, next) => {
+router.get('/', (req, res, next) => {
   contacts.getUsersContacts(req.session.passport.user.id)
   .then((userContacts) => {
     res.render('selectcontact', { contacts: userContacts })
@@ -15,8 +15,30 @@ router.get('/send', (req, res, next) => {
   })
 })
 
-router.post('/selectcontact', (req, res, next) => {
-  console.log(req.body)
+router.post('/amount', (req, res, next) => {
+  // req.session.passport.transaction = {
+  //   receiver: req.body.contact
+  // }
+
+  // switch(req.body.deliverymethod) {
+  //   case 'bank_transfer':
+  //     transaction.handling_fee = 10.00
+  //     break
+  //   case 'hand_delivery':
+  //     transaction.handling_fee = 15.00
+  //     break
+  //   default:
+  //     console.error('Invalid delivery method specified')
+  //     Raven.captureException(err, { user: { loggedInUser: req.session.passport.user, transaction: req.body } })
+  // }
+  contacts.getContactByNickname(req.session.passport.user.id, req.body.contact)
+  .then((contact) => {
+    res.render('transaction_details', { contact: contact })
+  })
+})
+
+router.post('/send/confirmation', (req, res, next) => {
+  res.send('money')
 })
 
 module.exports = router

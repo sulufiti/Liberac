52835e3ba0ee7e4974cecde49555cd58b1c6f4dc
Marcus Crane <marcus@thingsima.de>
2017-04-28 10:49:32 +1200

    Started filling out new migration and receipt

---
+++ migrations/20170428103751_transactions.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('transactions', (table) => {
+    table.uuid('id').primary()
+    table.uuid('sender').notNullable()
+    table.uuid('receiver').notNullable()
+    table.timestamp('transaction_time').defaultTo(knex.fn.now())
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('transactions')
+}

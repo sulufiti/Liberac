b45a1bda63922bf2baf6118012055470665034f7
Marcus Crane <marcus@thingsima.de>
2017-05-09 11:55:15 +1200

    Stripe script isn't inline anymore

---
+++ public/js/stripe.js
@@ -0,0 +1,54 @@
+var handler = StripeCheckout.configure({
+  key: 'pk_test_TltGDaGUDuxznboucS0lI2z6',
+  image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+  name: 'Liberac Ltd',
+  currency: 'nzd',
+  panelLabel: 'Send',
+  email: 'marcus@thingsima.de',
+  allowRememberMe: true,
+  description: 'Send funds to USER',
+  locale: 'auto',
+  zipCode: true,
+  token: function(token) {
+    fetch("/charge", {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ stripe: token, receiver: $('#receiver_name').val(), account: $('#bank_account').val() })
+    })
+    .then((output) => {
+      if (output.status === "succeeded") {
+        console.log('Purchase succeeded')
+        console.log(output)
+      }
+    })
+    .catch((err) => {
+      console.error('Purchase failed')
+      console.error(err)
+    })
+  }
+})
+
+$('#paymentButton').on('click', function(e) {
+  e.preventDefault()
+
+  $('#error_explanation').html('')
+
+  var amount = $('input#amount_to_send').val()
+
+  amount = amount.replace(/\$/g, '').replace(/\,/g, '')
+  amount = parseFloat(amount)
+
+  if (isNaN(amount)) {
+    $('#error_explanation').html('<p>Please go back and enter an amount to send.</p>');
+  }
+  else {
+    amount = amount * 100; // Needs to be an integer!
+    handler.open({
+      amount: Math.round(amount)
+    })
+  }
+})
+
+window.addEventListener('popstate', function() {
+  handler.close()
+})

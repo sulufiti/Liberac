15b91b57c9d710f0ff8a4777837d5d9bda49f1b9
Marcus Crane <marcus@thingsima.de>
2017-05-10 10:13:32 +1200

    Swapped back to non-unique email until we alert users or something

--- migrations/20170510095416_contact.js
+++ migrations/20170510095416_contact.js
@@ -3,7 +3,7 @@ exports.up = function (knex, Promise) {
     table.increments('id').primary()
     table.string('first_name').notNullable()
     table.string('last_name').notNullable()
-    table.string('email').notNullable().unique()
+    table.string('email').notNullable()
     table.timestamp('received').defaultTo(knex.fn.now())
   })
 }
